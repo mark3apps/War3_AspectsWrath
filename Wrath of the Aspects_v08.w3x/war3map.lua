84d16b652d566b67264fa593ba0a2ce7ed635640
mark3apps <mark.wright.vfx@gmail.com>
2020-09-27 15:51:53 -0700

    Updated

--- Wrath of the Aspects_v08.w3x/war3map.lua
+++ Wrath of the Aspects_v08.w3x/war3map.lua
@@ -1225,6 +1225,16 @@ function distance (x1, y1, x2, y2)
     return SquareRoot(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)))
 end
 
+
+function debugfunc( func, name )
+  local passed, data = pcall( function() func() return "func " .. name .. " passed" end )
+  if not passed then
+    print(name, passed, data)
+  end
+  passed = nil
+  data = nil
+end
+
 function AI_MAIN()
 
 
