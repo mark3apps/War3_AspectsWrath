78a82518300db12bfb140edd3a2763fed07ae5dc
Mark Wright <mark@chapeaufx.com>
2020-09-27 21:01:54 -0700

    Removed Debug

--- Warcraft_Lua/AI_Class.lua
+++ Warcraft_Lua/AI_Class.lua
@@ -618,7 +618,9 @@ function InitTrig_AI_Spell_Start()
 
 	print(CountUnitsInGroup(mapAI.heroGroup))
 
-	TriggerAddCondition(t, Condition(IsUnitInGroup(GetTriggerUnit(), mapAI.heroGroup)))
+	TriggerAddCondition(t, Condition( function()
+		IsUnitInGroup(GetTriggerUnit(), mapAI.heroGroup)
+	end))
 	
 	TriggerAddAction(t, function()
 		local hero = self[GetUnitUserData(GetTriggerUnit())]
