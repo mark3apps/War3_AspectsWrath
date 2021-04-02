8bfc1ddb246e7d80dc511d77acbfdf0615675300
mark3apps <mark.wright.vfx@gmail.com>
2021-04-01 22:11:31 -0700

    Updated

--- rr_lua/classes.lua
+++ rr_lua/classes.lua
@@ -8,13 +8,30 @@
 --  * means optional variable
 --
 --  Add new things to the fold
---  [ ] ai.addTown(name, force, {states})                           -- Adds a new town to the map.  (NEEDS to be extended with additional RECTs)
---  [ ] ai.addLandmark(town, {types}, name, unit, rect, rectLookAt, maxCapacity)   -- Sets up a new landmark in a town that will be used by Units
---  [ ] ai.addUnit(town, type, unit, name, shift*)                  -- Adds a unit into the fold to be controlled by the AI. Defaults to Day shift.
+--  [ ] ai.addTown(name, force)                           -- Adds a new town to the map.  (NEEDS to be extended with additional RECTs)
+--  [ ] ai.addLandmark(town, {types}, name, unit, rect, rectLookAt, radias, maxCapacity)   -- Sets up a new landmark in a town that will be used by Units
+--  [ ] ai.addUnit(town, type, unit, name, shift*)                  -- Adds a unit that exists into the fold to be controlled by the AI. Defaults to Day shift.
 --
 --  Extend Town Size
 --  [ ] ai.extendTown(name, rect)                -- Adds an additional RECT to a town that already exists
+--
+--  Town Actions
+--  [ ] ai.townState(name, state)               -- Changes the town state to the specified state
+--  [ ] ai.townVulnerable(name)                 -- Sets all units and landmarks of a town to be vulnerable.
+--  [ ] ai.townInvulnerable(name)               -- Sets all units and landmarks of a town to be invulnerable.
+--  [ ] ai.townHurtUnits(name, low, high)       -- Damages all units of the town by a random percentage from low to high.
+--  [ ] ai.townHurtLandmarks(name, low, high)   -- Damages all landmarks of the town by a random percentage from low to high.
+--  [ ] ai.townAddUnits(name, amount, unitType) -- Adds additional units of unit type to the town and places them at a random residence.
 
+--  Landmark Actions
+--  [ ] ai.landmarkState(rect, state)           -- Changes the landmark state to the specified state
+--  [ ] ai.landmarkKill(rect)                   -- Kills the landmark, displacing any Units connected to it.
+--
+--  Unit Actions
+--  [ ] ai.unitState(unit, state)               -- Changes the unit state to the specified state
+--  [ ] ai.unitKill(unit)                       -- Kills the unit
+--  [ ] ai.unitPause(unit, unitLookAt)          -- Stops the unit and tells it to look at a unit.
+--
 --  Set Unit Types
 --  [ ] ai.unitVillager(unit)                   -- Set unit as a Villager
 --  [ ] ai.unitMilitia(unit, unitTypeTransform) -- Set unit as a Militia
@@ -27,16 +44,21 @@
 --STATES
 --
 --  Town States
---  [ ] normal      -- The town is reporting everything is fine.
---  [ ] danger      -- The town is reporting that it is in Danger.
+--  [ ] auto        -- The town will automatically choose what state it should be in.
+--  [ ] normal      -- *AUTO* The town is reporting everything is fine.
+--  [ ] danger      -- *AUTO* The town is reporting that it is in danger.
+--  [ ] pause       -- The town will tell all of it's residences to pause and wait.
 --  [ ] gather      -- The town is reporting that everyone needs to gather in the town center
 --  [ ] abandon     -- The town is reporting that everyone should Abandon the Town.
 --
 --  Landmark States
---  [ ] normal      -- The Landmark is not reporting anything wierd
---  [ ] danger      -- The Landmark is reporting danger around it's perimeter
+--  [ ] auto        -- The Landmark will automatically choose what state to be in.
+--  [ ] normal      -- *AUTO* The Landmark is not reporting anything wierd.
+--  [ ] danger      -- *AUTO* The Landmark is reporting danger
+--  [ ] off         -- The Landmark is turned off and won't influence any behavior.
 --
 --  Unit States Available
+--  [ ] auto        -- Unit will automatically choose what state to be in.
 --  [ ] relax       -- Unit will stay where it's at and maybe run some specific animations.
 --  [ ] move        -- Unit will move around to a different spot inside it's current town.
 --  [ ] travel      -- Unit will travel to a new town.
@@ -48,7 +70,7 @@
 --  [ ] guard       -- Unit will stand in a position and will guard it always during his shift.
 --  [ ] attack      -- Unit will attack the specified town.
 --  [ ] follow      -- Unit will follow it's attached unit
---  [ ] flee        -- Unit will try to run away. (Either to nearest safehouse, or the nearest allied Town)
+--  [ ] flee        -- Unit will try to run away. (Either to nearest safehouse, or will keep running to landmarks in town if no safehouses)
 --  [ ] arm         -- Unit will try to find a barracks and arm himself to fight.
 --
 --
@@ -56,16 +78,17 @@
 -----------
 --TYPES
 --
---  Landmark Types
+--  Landmark Types (Each landmark can be multiple types)
 --  [ ] residence   -- This landmark will allow Units to sleep in it.
 --  [ ] safehouse   -- This landmark will allow Units to Hide in it when Fleeing from Danger.
 --  [ ] barracks    -- This landmark can house soldiers to be deployed if the town is under attack.
+--  [ ] patrol      -- This landmark is used to allow units to patrol it.
 --  [ ] playarea    -- This landmark is an area to play in.
 --  [ ] gathering   -- This landmark is used to gather get the Units to gather together.
 --  [ ] sight       -- This landmark is a sight seeing area where Units will move to and hang out at.
 --
 --
---  Unit Types
+--  Unit Types (Each unit can only be one type at a time)
 --  [ ] villager    -- Ordinary villager.  Will go about business, will always run away, and never fight.  Slows down when hurt, won't leave town.
 --          {relax, move, gather, sleep, flee}
 --
@@ -92,7 +115,7 @@ function INIT_ai()
 
     ai.data = {}
 
-    function ai.addTown(name, rect, force)
+    function ai.addTown(name, force)
 
 
     end
@@ -103,12 +126,12 @@ function INIT_ai()
     end
 
 
-    function ai.addLandmark(name, town, unit, rect, maxCapacity)
+    function ai.addLandmark(town, types, name, unit, rect, rectLookAt, radias, maxCapacity)
 
     end
 
 
-    function ai.addUnit(town, type, shift, unit, name, unitFollow)
+    function ai.addUnit(town, type, unit, name, shift)
 
     end
 
